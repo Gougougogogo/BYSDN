d6d23e50caa6ef05130b7a18e97eefa609a72221
Kevin Xi (Beyondsoft) <v-kevxi@microsoft.com>
2016-04-21 17:33:46 +0800

    add layout.js (loading img and personal information)

--- BYSDN/Content/page/dashboard.js
+++ BYSDN/Content/page/dashboard.js
@@ -1,16 +1,61 @@
-﻿var person;
-//load user info
-window.onload = function () {
+﻿$(document).ready(function () {
+    setSummaryData();
+});
+
+function setSummaryData() {
     $.ajax({
-        url: '/Home/GetUserInfo',
+        url: "../Home/GetSummaryData",
+        method: 'Get',
+        dataType: 'json',
         cache: false,
+        data: {},
         success: function (e) {
-            person = e;
-            if (e) {
-                var imgPath = person.Img.replace("..","http://"+window.location.host );
-                $('#userImg').attr("src", imgPath);
-                $('#userImg1').attr("src", imgPath);
+            if (e.success) {
+                var dataSource = e.retData;
+                $("#summary").dxChart({
+                    dataSource: dataSource,
+                    commonSeriesSettings: {
+                        argumentField: "Day"
+                    },
+                    series: [
+                        { valueField: "Question", name: "Question Count", color: "#40bbea" },
+                        { valueField: "Answer", name: "Answer Count", color: "#cc3f44" },
+                    ],
+                    argumentAxis: {
+                        grid: {
+                            visible: true
+                        }
+                    },
+                    tooltip: {
+                        enabled: true,
+                        argumentPrecision: 2,
+                        precision: 2,
+                        customizeTooltip: function (point) {
+                            return {
+                                text: 'Day (' + point.argumentText + ') of this month\'s ' + point.seriesName + ' is ' + point.valueText
+                            };
+                        }
+                    },
+                    title: "Question and Answers summary",
+                    legend: {
+                        verticalAlignment: "bottom",
+                        horizontalAlignment: "center"
+                    },
+                    commonPaneSettings: {
+                        border: {
+                            visible: true,
+                            right: false
+                        }
+                    },
+                    animation: {
+                        duration: 2000,
+                        easing: 'linear'
+                    }
+                });
+            }
+            else {
+                layer.msg(e.retData);
             }
         }
     });
-};
+}
---
+++ BYSDN/Content/page/layout.js
@@ -0,0 +1,16 @@
+﻿var person;
+//load user info
+window.onload = function () {
+    $.ajax({
+        url: '/Home/GetUserInfo',
+        cache: false,
+        success: function (e) {
+            person = e;
+            if (e) {
+                var imgPath = person.Img.replace("..", "http://" + window.location.host);
+                $('#userImg').attr("src", imgPath);
+                $('#userImg1').attr("src", imgPath);
+            }
+        }
+    });
+};
